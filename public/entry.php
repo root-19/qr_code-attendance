4aaff6a84b86d1b9c59a6e53c29b4134abdbeeff
rens20 <wasieacuna@gmail.com>
2024-05-17 23:51:07 +0800

    first commit

---
+++ public/entry.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code Attendance</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+    <header class="bg-white shadow-md flex items-center justify-between py-4 px-6">
+        <h2 class="text-2xl font-bold text-gray-800">
+            <a href="#" class="text-green-500 ">QR Code Attendance</a>
+        </h2>
+        <nav class="flex items-center space-x-4">
+            <ul class="flex space-x-4">
+                <li><a href="#" class="text-gray-700 hover:text-green-600 font-bold">Login</a></li>
+                <li><a href="#" class="text-gray-700 hover:text-green-600 font-bold">Developers</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container mx-auto mt-8 px-6 flex justify-center items-center">
+        <div class="text-center mt-36">
+            <h3 class="text-xl font-semibold text-gray-800">About Registration</h3>
+            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
+            <button class="mt-4 px-6 py-3 bg-green-500 text-white rounded-md hover:bg-green-700">Register</button>
+        </div>
+    </div>
+</body>
+</html>
